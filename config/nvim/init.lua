dd888f1ddcbd9796b25f6f9cc071b5abd365915b
Pete Yandell <pete@notahat.com>
2024-12-26 16:45:41 +1100

    Improve Neovim config docs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,17 @@
+-- Highlights of this config are:
+--
+-- * Plugin management with lazy.nvim
+-- * Automatic installation of language servers and formatters with mason.nvim
+-- * Language server support with nvim-lspconfig
+-- * Auto-formatting with null-ls
+-- * Completion with nvim-cmp
+-- * Syntax highlighting with nvim-treesitter
+-- * File explorer with oil.nvim
+-- * Fuzzy finding of all sorts of things with Telescope
+--
+-- I keep the UI much more minimal than a normal IDE. I'm easily distracted, so
+-- I want my code front-and-centre with not too much other information.
+
 require("config.options")
 require("config.lazy")
 require("config.key-mappings")
