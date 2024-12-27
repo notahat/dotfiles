7e83c593b85f67eeb8d8fc4c74fcd06777ec6ce5
Pete Yandell <pete@notahat.com>
2024-12-27 18:10:35 +1100

    Darken Neovim selection background a bit

--- config/nvim/lua/plugins/nightfox.lua
+++ config/nvim/lua/plugins/nightfox.lua
@@ -6,6 +6,14 @@ return {
   config = function()
     local nightfox = require("nightfox")
     nightfox.setup({
+      groups = {
+        all = {
+          -- Darken selection to give more contrast with text.
+          -- This works nicely against a black background.
+          CursorLine = { bg = "#22262f" },
+          Visual = { bg = "#22262f" },
+        },
+      },
       options = { transparent = true },
     })
     vim.cmd.colorscheme("nordfox")
