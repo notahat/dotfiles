f76edf43749123ee4659c49604a17963e606104e
Pete Yandell <pete@notahat.com>
2025-01-04 20:08:57 +1100

    Show macro recording in lualine properly

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -6,24 +6,25 @@ local function copy_relative_path()
   vim.notify("Path copied: " .. path)
 end
 
-local function has_noice_mode()
-  require("noice").api.status.mode.has()
-end
-
-local function noice_mode()
-  require("noice").api.status.mode.get()
-end
-
-local function lsp_progess()
-  return require("lsp-progress").progress()
-end
-
 local function open_diagnostics()
   require("trouble").open("diagnostics")
 end
 
 local function show_lsp_info()
   vim.cmd("checkhealth lspconfig")
+end
+
+local function register_recording()
+  local register = vim.fn.reg_recording()
+  if register == "" then
+    return ""
+  else
+    return "recording @" .. register
+  end
+end
+
+local function lsp_progress()
+  return require("lsp-progress").progress()
 end
 
 return {
@@ -35,15 +36,12 @@ return {
     sections = {
       lualine_a = { "mode" },
       lualine_b = { { "diagnostics", on_click = open_diagnostics } },
-      lualine_c = { { "filename", on_click = copy_relative_path, path = 1, shorting_target = 20 } },
+      lualine_c = {
+        { "filename", on_click = copy_relative_path, path = 1, shorting_target = 20 },
+      },
       lualine_x = {
-        -- Show macro recording messages.
-        {
-          noice_mode,
-          cond = has_noice_mode,
-          color = { fg = "#ff9e64" },
-        },
-        { lsp_progess, on_click = show_lsp_info },
+        { register_recording, color = { fg = "#ff9e64" } },
+        { lsp_progress, on_click = show_lsp_info },
       },
       lualine_y = { "searchcount" },
       lualine_z = { "%p%%/%L" },
--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,10 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  dependencies = { "MunifTanjim/nui.nvim" },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
   opts = {
     lsp = {
       progress = { enabled = false },
