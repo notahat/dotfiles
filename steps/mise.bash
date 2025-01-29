d34f3ae9fe80918f5deeb92f1797bbef7c5baf09
Pete Yandell <pete@notahat.com>
2025-01-30 10:16:49 +1100

    Don't install mise at work

--- steps/mise.bash
+++ steps/mise.bash
@@ -3,9 +3,11 @@
 mkdir -p ~/.config/mise
 link_file "environments/$DOTFILES_ENV/mise.toml" ~/.config/mise/config.toml
 
-# Make sure we've got homebrew loaded, coz mise is installed with it.
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ $DOTFILES_ENV == home ]]; then
+  # Make sure we've got homebrew loaded, coz mise is installed with it.
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 
-eval "$(mise activate bash)"
+  eval "$(mise activate bash)"
 
-mise install
+  mise install
+fi
