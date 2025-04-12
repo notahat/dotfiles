73d6af5bc7bb2996da8478c2feabd98d6448ed51
Pete Yandell <pete@notahat.com>
2025-04-12 21:09:07 +1000

    Move Neovim diagnostics config into its own file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.options")
+require("config.diagnostics")
 require("config.lazy")
 require("config.key-mappings")
 require("config.kitty-integration")
--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -26,24 +26,5 @@ return {
         },
       },
     })
-
-    -- Set the diagnostic signs shown in the gutter to match lualine's.
-    vim.diagnostic.config({
-      virtual_lines = { current_line = true },
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = "󰅚 ",
-          [vim.diagnostic.severity.WARN] = "󰀪 ",
-          [vim.diagnostic.severity.INFO] = "󰋽 ",
-          [vim.diagnostic.severity.HINT] = "󰌶",
-        },
-        texthl = {
-          [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-          [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-          [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-          [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-        },
-      },
-    })
   end,
 }
