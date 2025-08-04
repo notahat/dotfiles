c2fa1471605c2ece16d35274d0344cb455323b8d
Pete Yandell <pete@notahat.com>
2025-08-04 23:18:27 +1000

    Add custom Zed keymap for Copilot edit predictions

--- steps/zed.bash
+++ steps/zed.bash
@@ -1,5 +1,6 @@
 # shellcheck shell=bash
 
 mkdir -p ~/.config/zed
+link_file config/zed/keymap.json ~/.config/zed/keymap.json
 link_file config/zed/settings.json ~/.config/zed/settings.json
 link_file config/zed/tasks.json ~/.config/zed/tasks.json
