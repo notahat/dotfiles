d0597de820c790a3f20d049a104d73c72633b102
Pete Yandell <pete@notahat.com>
2025-12-02 11:10:19 +1100

    Update Mise install script to v2025.8.20 for work environment

--- steps/mise.bash
+++ steps/mise.bash
@@ -5,7 +5,7 @@ link_file "environments/$DOTFILES_ENV/mise.toml" ~/.config/mise/config.toml
 
 if [[ $DOTFILES_ENV == work ]]; then
   if [[ ! -f ~/.local/bin/mise ]]; then
-    curl https://mise.run | MISE_VERSION="v2024.12.7" sh
+    curl https://mise.run | MISE_VERSION="v2025.8.20" sh
   fi
 fi
 
