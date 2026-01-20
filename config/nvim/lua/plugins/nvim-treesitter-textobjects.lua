ba10db01fd4e7e5f71b2d96e5588089e63eba90e
Pete Yandell <pete@notahat.com>
2026-01-20 15:40:48 +1100

    Update to modern treesitter in Neovim.

---
+++ config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -0,0 +1,51 @@
+-- Manipulate text objects using treesitter.
+
+local function select_outer_block()
+  require("nvim-treesitter-textobjects.select").select_textobject("@block.outer", "textobjects")
+end
+
+local function select_inner_block()
+  require("nvim-treesitter-textobjects.select").select_textobject("@block.inner", "textobjects")
+end
+
+local function select_outer_function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+end
+
+local function select_inner_function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+end
+
+local function select_outer_parameter()
+  require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
+end
+
+local function select_inner_parameter()
+  require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "main",
+  init = function()
+    -- Disable entire built-in ftplugin mappings to avoid conflicts.
+    -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+    vim.g.no_plugin_maps = true
+
+    -- Or, disable per filetype (add as you like)
+    -- vim.g.no_python_maps = true
+    -- vim.g.no_ruby_maps = true
+    -- vim.g.no_rust_maps = true
+    -- vim.g.no_go_maps = true
+  end,
+  config = function()
+    require("nvim-treesitter-textobjects").setup({})
+
+    vim.keymap.set({ "x", "o" }, "ab", select_outer_block, { desc = "block" })
+    vim.keymap.set({ "x", "o" }, "ib", select_inner_block, { desc = "inner block" })
+    vim.keymap.set({ "x", "o" }, "af", select_outer_function, { desc = "function" })
+    vim.keymap.set({ "x", "o" }, "if", select_inner_function, { desc = "inner function" })
+    vim.keymap.set({ "x", "o" }, "ap", select_outer_parameter, { desc = "parameter" })
+    vim.keymap.set({ "x", "o" }, "ip", select_inner_parameter, { desc = "inner parameter" })
+  end,
+}
--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,59 +1,20 @@
 -- Provide syntax highlighting and textobjects through the treesitter parser.
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  branch = "main",
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "regex",
-        "vim",
-        "vimdoc",
-      },
-      ignore_install = {},
-      auto_install = true,
-      sync_install = false,
-      modules = {},
-
-      highlight = { enable = true },
-
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = {
-            ["]f"] = { query = "@function.outer", desc = "Next function" },
-            ["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
-          },
-          goto_previous_start = {
-            ["[f"] = { query = "@function.outer", desc = "Previous function" },
-            ["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
-          },
-        },
-        select = {
-          enable = true,
-          include_surrounding_whitespace = true,
-          keymaps = {
-            ["ab"] = { query = "@block.outer", desc = "block" },
-            ["ib"] = { query = "@block.inner", desc = "inner block" },
-            ["af"] = { query = "@function.outer", desc = "function" },
-            ["if"] = { query = "@function.inner", desc = "inner function" },
-            ["ap"] = { query = "@parameter.outer", desc = "parameter" },
-            ["ip"] = { query = "@parameter.inner", desc = "inner parameter" },
-          },
-        },
-      },
+    require("nvim-treesitter").install({
+      "bash",
+      "html",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "regex",
+      "vim",
+      "vimdoc",
     })
-
-    local repeatable_move = require("nvim-treesitter.textobjects.repeatable_move")
-    vim.keymap.set({ "n", "x", "o" }, ";", repeatable_move.repeat_last_move_next)
-    vim.keymap.set({ "n", "x", "o" }, ",", repeatable_move.repeat_last_move_previous)
   end,
 }
