07deb875115849c6180242ff0e9da4c5b11f1ebf
Pete Yandell <pete@notahat.com>
2026-04-18 15:00:52 +1000

    Strip down my zed config.
    I'm going to use project-specific config for more things.

--- steps/zed.bash
+++ steps/zed.bash
@@ -3,4 +3,3 @@
 mkdir -p ~/.config/zed
 link_file config/zed/keymap.json ~/.config/zed/keymap.json
 link_file config/zed/settings.json ~/.config/zed/settings.json
-link_file config/zed/tasks.json ~/.config/zed/tasks.json
